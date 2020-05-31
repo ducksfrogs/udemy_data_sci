4bb0b15a951894995120202c47c8e7121534f4b6
ma <ma_yamaki@yahoo.com>
2020-05-31 16:24:21 +0900

    template

--- R/regressionTemplate.R
+++ R/regressionTemplate.R
@@ -6,11 +6,12 @@ lin_reg <- lm(formula = Salary ~ ., data = dataset)
 
 
 library(ggplot2)
+X_grid <- seq(min(dataset$Level), max(dataset$Level), 0.1)
 ggplot() + geom_point(aes(x= training_set$YearsExperience,
                           y= training_set$Salary),
-                      color='red') + 
-          geom_line(aes(x=training_set$YearsExperience,
-                        y=predict(regressor, newdata = training_set) )
+                      color='red') +
+          geom_line(aes(x=X_grid,
+                        y=predict(regressor, newdata = data.frame(Level = X_grid)) )
                     ,color='blue') +
           ggtitle("Salary vs Expericne")+
           xlab("Years Experience") +
